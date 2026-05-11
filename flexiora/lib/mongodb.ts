1af453ed000c3c92d79e86491e9041af628e075d
thearnab05 <arnabsural1890@gmail.com>
2026-05-11 19:03:34 +0530

    feat: implement MongoDB connection utility with global caching for Mongoose

--- flexiora/lib/mongodb.ts
+++ flexiora/lib/mongodb.ts
@@ -8,11 +8,7 @@ if (!MONGODB_URI) {
   );
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
+
 let cached = (global as any).mongoose;
 
 if (!cached) {
